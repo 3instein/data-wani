1b9171464b6aa08dff4b34d18998c17b8e1417b4
3instein <reynaldi_kindarto@yahoo.com>
2023-04-13 16:58:22 +0700

    Latest update

--- deploy.py
+++ deploy.py
@@ -104,5 +104,5 @@ def handle_message(event):
 
 import os
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 80))
-    app.run(host='localhost', port=port)
+    # port = int(os.environ.get('PORT', 80))
+    app.run(host='localhost', port=80)
