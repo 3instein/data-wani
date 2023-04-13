48e717910c0510797975786fca1b54195807437a
3instein <reynaldi_kindarto@yahoo.com>
2023-04-13 15:50:37 +0700

    Fix callback on webhook

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, request, abort
+import pandas as pd
+import gspread
+import requests
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -18,17 +21,80 @@ handler = WebhookHandler('cb15d13e97326a3349cb158e03846af1')
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    return 'OK'
+    token = 'AF5y90PW7vmJALond4SEGVBFwmt3Jhj7Ba7oclAeXL4'
+
+    sa = gspread.service_account(filename='geprek-wani-c700b4b8002f.json')
+
+    sheet = sa.open('Wani Spreadsheet')
+
+    worksheet = sheet.worksheet('Pesanan')
+
+    df = pd.DataFrame(worksheet.get_all_records())
+
+    df.rename(columns={'Request cabe GEPREK': 'Cabe'}, inplace=True)
+
+    df.head()
+
+    # create a list containing 'Makanan Utama' and 'Nama'
+    list = ['Makanan Utama', 'Cabe', 'Nama']
+
+    # replace the empty string with 1
+    df[list] = df[list].replace('', 1)
+
+    df[list]
+
+    # Custom geprek
+    df_geprek = df[df['Makanan Utama'].str.contains('Geprek') & ~df['Makanan Utama'].str.contains('Indomie')]
+    df_geprek
+
+    # Dataframe non-geprek
+    df_not_geprek = df[~df['Makanan Utama'].str.contains('Geprek')]
+
+    # Dataframe geprek tanpa nasi
+    df_geprek_tanpa_nasi = df_geprek[df_geprek['Makanan Utama'].str.contains('tanpa nasi')]
+
+    # Dataframe geprek dengan nasi
+    df_geprek_dengan_nasi = df_geprek[~df_geprek['Makanan Utama'].str.contains('tanpa nasi')]
+
+    # Dataframe indomie geprek
+    df_indomie_geprek = df[df['Makanan Utama'].str.contains('Indomie') & df['Makanan Utama'].str.contains('Geprek')]
+
+    # Dataframe camilan
+    df_camilan = df[['Camilan', 'Nama']]
+    df_camilan = df_camilan[df_camilan['Camilan'] != '']
+
+    # Dataframe minuman
+    df_minuman = df[['Minuman', 'Nama']]
+    df_minuman = df_minuman[df_minuman['Minuman'] != '']
+
+    # Dataframe sambal
+    df_sambal = df[['Sambal', 'Nama']]
+    df_sambal = df_sambal[df_sambal['Sambal'] != '']
+
+    combined_prints = []
+    combined_prints.append(f"Pesanan Depot Wani: {pd.Timestamp.now()}")
+    combined_prints.append("=====================================")
+    combined_prints.extend([f"{sum(df_geprek_dengan_nasi['Cabe'] == i)} Nasi Ayam Geprek Cabe {i} ({', '.join(df_geprek_dengan_nasi[df_geprek_dengan_nasi['Cabe'] == i]['Nama'])})" for i in df_geprek_dengan_nasi['Cabe'].unique()])
+    combined_prints.extend([f"{sum(df_geprek_tanpa_nasi['Cabe'] == i)} Ayam Geprek tanpa nasi Cabe {i} ({', '.join(df_geprek_tanpa_nasi[df_geprek_tanpa_nasi['Cabe'] == i]['Nama'])})" for i in df_geprek_tanpa_nasi['Cabe'].unique()])
+    combined_prints.extend([f"{sum(df_not_geprek['Makanan Utama'] == i)} {i} ({', '.join(df_not_geprek[df_not_geprek['Makanan Utama'] == i]['Nama'])})" for i in df_not_geprek['Makanan Utama'].unique()])
+    combined_prints.extend([f"{sum(df_indomie_geprek['Cabe'] == i)} {df_indomie_geprek[df_indomie_geprek['Cabe'] == i]['Makanan Utama'].iloc[0]} Cabe {i} ({', '.join(df_indomie_geprek[df_indomie_geprek['Cabe'] == i]['Nama'])})" for i in df_indomie_geprek['Cabe'].unique()])
+    combined_prints.extend([f"{sum(df_camilan['Camilan'] == i)} {i} ({', '.join(df_camilan[df_camilan['Camilan'] == i]['Nama'])})" for i in df_camilan['Camilan'].unique()])
+    combined_prints.extend([f"{sum(df_minuman['Minuman'] == i)} {i} ({', '.join(df_minuman[df_minuman['Minuman'] == i]['Nama'])})" for i in df_minuman['Minuman'].unique()])
+    combined_prints.extend([f"{sum(df_sambal['Sambal'] == i)} {i} ({', '.join(df_sambal[df_sambal['Sambal'] == i]['Nama'])})" for i in df_sambal['Sambal'].unique()])
+
+    print('\n'.join(combined_prints))
+
+    # remove all text after '(' in combined_prints
+    # combined_prints = [i.split('(')[0] for i in combined_prints]
+
+    # print('\n'.join(combined_prints))
+
+    # payload = {'message' : '\n'.join(combined_prints)}
+    # r = requests.post('https://notify-api.line.me/api/notify'
+    #                 , headers={'Authorization' : 'Bearer {}'.format(token)}
+    #                 , params = payload)
+    
+    return 'Success'
 
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
@@ -38,5 +104,6 @@ def handle_message(event):
 
 import os
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
+    app.debug = True
+    port = int(os.environ.get('PORT', 5001))
     app.run(host='0.0.0.0', port=port)
