f2af6d5acf069487b96abcefc0ea639186b1fcd4
3instein <reynaldi_kindarto@yahoo.com>
2023-05-25 13:15:27 +0700

    Change ssl_context to adhoc

--- app.py
+++ app.py
@@ -127,4 +127,4 @@ import os
 if __name__ == "__main__":
     app.debug = True
     port = int(os.environ.get('PORT', 80))
-    app.run(host='0.0.0.0', port=port, ssl_context=('cert.pem', 'key.pem'))
+    app.run(host='0.0.0.0', port=port, ssl_context='adhoc')
